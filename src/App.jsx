88b1e782cb60ba6e807426bf9110a4139b1acd32
Shubham Maheshwari <smshubham54321@gmail.com>
2025-03-02 18:29:48 +0530

    updated dependency

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,29 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Importing Router components
-import HomePage from './pages/HomePage';
-import Propose2Page from './pages/Propose2Page';
-import Propose3Page from './pages/Propose3Page';
-import Propose4Page from './pages/Propose4Page';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"; // Importing Router components
+import HomePage from "./pages/HomePage";
+import Propose2Page from "./pages/Propose2Page";
+import Propose3Page from "./pages/Propose3Page";
+import Propose4Page from "./pages/Propose4Page";
 
 function App() {
   console.log("App chala");
   return (
     // <HomePage/>
-    <Router>
-      <div>
-        {/* Define Routes */}
-        <Routes>
-          <Route path="/" element={<HomePage />} /> {/* HomePage route */}
-          <Route path="/propose2" element={<Propose2Page />} /> {/* Propose2Page route */}
-          <Route path="/propose3" element={<Propose3Page />} /> {/* Propose3Page route */}
-          <Route path="/propose4" element={<Propose4Page />} /> {/* Propose4Page route */}
-        </Routes>
-      </div>
-    </Router>
+    <BrowserRouter>
+      <Router>
+        <div>
+          {/* Define Routes */}
+          <Routes>
+            <Route path="/" element={<HomePage />} /> {/* HomePage route */}
+            <Route path="/propose2" element={<Propose2Page />} />{" "}
+            {/* Propose2Page route */}
+            <Route path="/propose3" element={<Propose3Page />} />{" "}
+            {/* Propose3Page route */}
+            <Route path="/propose4" element={<Propose4Page />} />{" "}
+            {/* Propose4Page route */}
+          </Routes>
+        </div>
+      </Router>
+    </BrowserRouter>
   );
 }
 
